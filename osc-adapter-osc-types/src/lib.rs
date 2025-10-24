bca600e3f6cf2548c6d0daf89c5df0d62cacc933
Yu Kusanagi <kusanagi.y@mujina-prod.com>
2025-10-24 05:35:00 +0000

    osc1.1 treat as TODOs

--- osc-adapter-osc-types/src/lib.rs
+++ osc-adapter-osc-types/src/lib.rs
@@ -6,12 +6,14 @@ pub mod v10 {
     use osc_types10 as osc;
 
     pub fn message_to_ir(_m: &osc::OscMessage) -> IrValue {
-        // TODO: map OSC 1.0 message to IrValue (path, args, etc.)
+        // TODO: Implement OSC 1.0 message to IrValue conversion
+        // This should map OSC message path and arguments to appropriate IrValue structure
         IrValue::Null
     }
 
     pub fn ir_to_message(_v: &IrValue) -> Option<osc::OscMessage> {
-        // TODO: reconstruct message if representable
+        // TODO: Implement IrValue to OSC 1.0 message conversion
+        // This should reconstruct message if representable in OSC 1.0 format
         None
     }
 }
@@ -20,5 +22,16 @@ pub mod v10 {
 pub mod v11 {
     use super::*;
     use osc_types11 as osc;
-    // TODO: same as v10 for OSC 1.1
+    
+    pub fn message_to_ir(_m: &osc::OscMessage) -> IrValue {
+        // TODO: Implement OSC 1.1 message to IrValue conversion
+        // This should handle all OSC 1.1 types including Color and MIDI
+        IrValue::Null
+    }
+
+    pub fn ir_to_message(_v: &IrValue) -> Option<osc::OscMessage> {
+        // TODO: Implement IrValue to OSC 1.1 message conversion
+        // This should handle OSC 1.1 specific types like Color and MIDI
+        None
+    }
 }
